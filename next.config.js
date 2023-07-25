b76cf8c386de7b424cd9cf8c58b43bf609f05abb
Gilmar Alves <GilmarAlves914@gmail.com>
2023-07-25 12:47:02 -0300

    Feat: add trip

--- next.config.js
+++ next.config.js
@@ -2,11 +2,18 @@
 const nextConfig = {
     images: {
         remotePatterns: [{
-            protocol: 'https',
-            hostname: 'lh3.googleusercontent.com',
-            port: '',
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
 
-        }, ],
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+
+            },
+        ],
     }
 }
 
